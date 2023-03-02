df123606a02f507354835afa6eba5dd4e4bb55a7
Christnguci <bestprofull@gmail.com>
2023-03-02 17:37:14 +0700

    Dectect wall, pop up score, ask user wants to play ?

--- main.py
+++ main.py
@@ -3,52 +3,89 @@ import time
 from snake import Snake
 from food import Food
 import math
-colormode(255)
-
+import tkinter as tk
+from tkinter import messagebox
 screen=Screen()
 screen.setup(600,600)
 screen.title("My snake")
 screen.bgcolor("black")
-snake=Snake()
-food=Food()
-
-screen.listen()
-screen.onkey(key="w",fun=snake.up)
-screen.onkey(key="a",fun=snake.left)
-screen.onkey(key="d",fun=snake.right)
-screen.onkey(key="s",fun=snake.down)
-screen.tracer(0) 
-
-def distance():
-    x= int(math.sqrt((snake.x_head()-food.x_food())*(snake.x_head()-food.x_food())+(snake.y_head()-food.y_food())*(snake.y_head()-food.y_food())))
-    return x
-eat_times=0
+def create_a_snake ():
+    snake=Snake()
+    return snake
+def create_food():
+    food=Food()
+    return food
+#Updatescore
 Score=Turtle()
 Score.color("white")
-keep_forward=True
-while keep_forward:
-    screen.update()
-    time.sleep(0.07)
-    snake.move()  
-    if distance()<15:
-        food.move()
-        snake.add()
-        eat_times+=1
-        Update_score()
-        if eat_times%5==0:
-            food.shapesize(2)
+def Update_score (eat_times):
+    Score.clear()
+    Score.penup()
+    Score.goto(200, 200)
+    Score.pendown()
+    font = ("Arial", 10,"normal")
+    Score.write(f"Your score: {eat_times}",font=font)
+    Score.hideturtle()
+def distance(snake,food):
+    x= int(math.sqrt((snake.x_head()-food.x_food())*(snake.x_head()-food.x_food())+(snake.y_head()-food.y_food())*(snake.y_head()-food.y_food())))
+    return x
+
+
+def deadth(snake):
+    x=int(math.sqrt((snake.x_head()*snake.x_head())))
+    y=int(math.sqrt((snake.y_head()*snake.y_head())))
+    if x>305 or y >305 :
+        return True
+    return False
+un_deadth=True
+
+def Play_game():
+    snake=create_a_snake()
+    food=create_food()
+    eat_times=0
+    screen.onkey(key="w",fun=snake.up)
+    screen.onkey(key="a",fun=snake.left)
+    screen.onkey(key="d",fun=snake.right)
+    screen.onkey(key="s",fun=snake.down)
+    screen.tracer(0)
+    keep_forward=True
+    while keep_forward:
+            screen.update()
+            time.sleep(0.07)
+            snake.move()  
+            if distance(snake,food)<15:
+                food.move()
+                snake.add()
+                eat_times+=1
+                Update_score(eat_times)
+                if eat_times%5==0:
+                    food.shapesize(2)
+                else:
+                    food.shapesize(1)
+            if deadth(snake):
+                        # Create a Tkinter root window
+                        root = tk.Tk()
+
+                        # Hide the root window
+                        root.withdraw()
+
+                        # Show a pop-up message box with a message and an OK button
+                        messagebox.showinfo("Score",f"Your final score is {eat_times}.")
+
+                        # Destroy the root window
+                        root.destroy()
+                        Turtle.reset()
+                        break
+            
+                    
+
+while un_deadth:
+        user_answer=messagebox.askokcancel("Continue ?"," Do you want to play the game ? ")
+        if user_answer:
+            screen.listen()
+            Play_game()
         else:
-            food.shapesize(1)
-#Updatescore
-    def Update_score ():
-        Score.clear()
-        Score.penup()
-        Score.goto(200, 200)
-        Score.pendown()
-        font = ("Arial", 10,"normal")
-        Score.write(f"Your score: {eat_times}",font=font)
-        Score.hideturtle()
+            break
 
 
-screen.exitonclick()
 #hello
--- test.py
+++ test.py
@@ -1,65 +1,13 @@
-from turtle import Turtle, Screen
+import tkinter as tk
+from tkinter import messagebox
 
+# Create a Tkinter root window
+root = tk.Tk()
 
-screen=Screen()
-screen.setup(600,600)
-screen.title("My snake")
-#screen.tracer(n)-screen is update after n movements of turtle
-#screen.tracer(n,delay(m))-n movements of turtle and delay m milisecond 
-screen.tracer(0)
-starting_positions=[(0,0),(-20,0),(-40,0)]
-segments=[]
-for position in starting_positions:
-    new_segment =Turtle("square")
-    new_segment.color("black")
-    new_segment.goto(position)
-    segments.append(new_segment)
+# Hide the root window
 
-#movement
-def up():
-    segments[0].setheading(90)
-def left():
-    segments[0].setheading(180)
-def down():
-    segments[0].setheading(270)
-def right():
-    segments[0].setheading(0)
-def clear():
-    segments.reset() 
-import time
-keep_forward=True
-
-while keep_forward:
-    screen.update()
-    time.sleep(0.07)
-    for seg_num in range(len(segments)-1,0,-1):
-         new_x=segments[seg_num-1].xcor()
-         new_y=segments[seg_num-1].ycor()
-         segments[seg_num].goto(new_x,new_y)
-         segments[seg_num].clear()
-    
-    segments[0].forward(10)
-    segments[0].clear()
-    
-
-        
-    screen.listen()
-    screen.onkey(key="w",fun=up)
-    screen.onkey(key="a",fun=left)
-    screen.onkey(key="d",fun=right)
-    screen.onkey(key="s",fun=down)
-
-
-
-        
-   
-
-
-
-
-    
-
-
-    
-screen.onkey(key="c",fun=clear)
-screen.exitonclick()
+# Show a pop-up message box with a message and an OK button
+x=messagebox.askokcancel("Message", "This is a pop-up message box. Click OK to continue.")
+print(x)
+# Destroy the root window
+root.destroy()
